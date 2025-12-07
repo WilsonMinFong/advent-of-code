5de7eea0818d31c4092e101d19ddd3c2e29024b6
Wilson Fong <wilsonmfong@gmail.com>
2025-12-07 09:58:19 -0500

    Solve Day 7, Part 2

--- 2025/day7/day7.rb
+++ 2025/day7/day7.rb
@@ -26,6 +26,36 @@ def get_num_beam_splits(filename)
   num_beam_splits
 end
 
+def get_num_timelines(filename)
+  current_beam_positions = {}
+  new_beam_positions = Hash.new(0)
+
+  File.readlines(filename, chomp: true).each_with_index do |line, line_num|
+    if line_num == 0
+      current_beam_positions = { line.chars.find_index('S') => 1}
+    else
+      new_beam_positions = Hash.new(0)
+
+      current_beam_positions.each do |current_beam_position, n_timelines_to_current_beam_position|
+        if line[current_beam_position] == '^'
+          new_beam_positions[current_beam_position - 1] += n_timelines_to_current_beam_position unless current_beam_position == 0
+          new_beam_positions[current_beam_position + 1] += n_timelines_to_current_beam_position unless current_beam_position == line.length - 1
+        else
+          new_beam_positions[current_beam_position] += n_timelines_to_current_beam_position
+        end
+      end
+
+      current_beam_positions = new_beam_positions
+    end
+  end
+
+  new_beam_positions.values.reduce(&:+)
+end
+
 puts "=== Part 1 ==="
 puts "Example solution: #{ get_num_beam_splits('./example_input.txt')}"
 puts "Solution: #{get_num_beam_splits('./input.txt')}"
+
+puts "=== Part 2 ==="
+puts "Example solution: #{ get_num_timelines('./example_input.txt')}"
+puts "Solution: #{get_num_timelines('./input.txt')}"
