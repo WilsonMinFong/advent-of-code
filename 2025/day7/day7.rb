fe3516cf95714064897da0b146da411b16310832
Wilson Fong <wilsonmfong@gmail.com>
2025-12-07 08:50:48 -0500

    Solve Day 7, Part 1

---
+++ 2025/day7/day7.rb
@@ -0,0 +1,31 @@
+def get_num_beam_splits(filename)
+  current_beam_positions = []
+  num_beam_splits = 0
+
+  File.readlines(filename, chomp: true).each_with_index do |line, line_num|
+    if line_num == 0
+      current_beam_positions = [line.chars.find_index('S')]
+    else
+      new_beam_positions = []
+
+      current_beam_positions.each do |current_beam_position|
+        if line[current_beam_position] == '^'
+          num_beam_splits += 1
+
+          new_beam_positions << current_beam_position - 1 unless current_beam_position == 0
+          new_beam_positions << current_beam_position + 1 unless current_beam_position == line.length - 1
+        else
+          new_beam_positions << current_beam_position
+        end
+      end
+
+      current_beam_positions = new_beam_positions.uniq
+    end
+  end
+
+  num_beam_splits
+end
+
+puts "=== Part 1 ==="
+puts "Example solution: #{ get_num_beam_splits('./example_input.txt')}"
+puts "Solution: #{get_num_beam_splits('./input.txt')}"
